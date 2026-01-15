8e05cfc0bea6dbb8ef2c7be11c17e55b478abd28
Yuval <yuvalkat@gmail.com>
2026-01-15 18:36:56 +0200

    Gets comments by post id

---
+++ model/postModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { commentSchema } = require("./commentModel");
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  comments: [commentSchema],
+  upload: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+const postModel = mongoose.model("posts", postSchema);
+
+module.exports = { postModel };
